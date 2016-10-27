062297f18c0f6f4307550c50498fa8e4b268544f
kpavlov <git@konstantinpavlov.net>
2016-10-28 02:44:37 +0300

    gulp-babel, sourcemaps

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,18 +1,22 @@
-'use strict'
+'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var config = require('../config').scripts;
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const config = require('../config').scripts;
 
 function process(cfg) {
     return gulp.src(cfg.src)
-        // .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
         .pipe(concat(cfg.fileName, {newLine: ';'}))
         .pipe(gulp.dest(config.dest))
         .pipe(uglify())
-        // .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dest));
 }
 
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,16 +1,23 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minify = require('gulp-clean-css');
-var config = require('../config.js');
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const minify = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const config = require('../config.js');
 
 gulp.task('styles', function () {
     gulp.src(config.sass.src)
+        .pipe(sourcemaps.init())
         .pipe(sass(config.sass.settings))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.sass.dest));
 
     gulp.src(config.css.src)
+        .pipe(sourcemaps.init())
         .pipe(concat(config.css.fileName))
         .pipe(minify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.css.dest))
 });
